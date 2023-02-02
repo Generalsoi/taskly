295398d6222e92e69a2d42166fb6286cef4f04f1
Generalsoi <ikhinobelesuccess@gmail.com>
2023-02-03 00:39:30 +0100

    fix: created a vite application and installed typescript in it

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div className="">
+      <span className="text-center p-10">
+        <h1 className="font-unbounded text-2xl md:text-4xl font-bold">
+          Taskly
+        </h1>
+      </span>
+    </div>
+  );
+};
+
+export default App;
