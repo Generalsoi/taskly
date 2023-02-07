c39858289d0076860ebf652486fb1a89b499dce0
Generalsoi <ikhinobelesuccess@gmail.com>
2023-02-07 23:53:19 +0100

    fix: completed taskly

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,6 +1,5 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { Todo } from "./model";
-import { BiEdit } from "react-icons/bi";
 import { AiOutlineDelete } from "react-icons/ai";
 import { IoCheckmarkDoneCircle } from "react-icons/io5";
 
@@ -10,16 +9,40 @@ interface Props {
 }
 
 export const TodoList: FC<Props> = ({ todos, setTodos }) => {
+  //
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
   return (
-    <div className="md:flex flex-wrap items-center justify-center gap-10 md:p-10">
+    <div className="md:flex flex-wrap items-center justify-center gap-10 p-6 md:p-10">
       {todos.map((todo) => (
-        <li className="w-full md:w-[30%] h-16 border-none rounded-lg bg-[#7B8FA1] text-white list-none flex items-center justify-between p-4">
-          {todo.todo}
-          <span className="flex items-center gap-3">
-            <BiEdit className="cursor-pointer" />
-            <AiOutlineDelete className="cursor-pointer" />
-            <IoCheckmarkDoneCircle className="cursor-pointer" />
-          </span>
+        <li
+          key={todo.id}
+          className={`w-full md:w-[30%] h-16 border-none rounded-lg ${
+            todo.isDone ? "bg-[#CFB997]" : "bg-[#7B8FA1]"
+          } text-white list-none p-4 mt-6 md:mt-0`}
+        >
+          <form className=" flex items-center justify-between">
+            {todo.isDone ? <s>{todo.todo}</s> : <span>{todo.todo}</span>}
+            <div className="flex items-center gap-3">
+              <AiOutlineDelete
+                className="cursor-pointer"
+                onClick={() => handleDelete(todo.id)}
+              />
+              <IoCheckmarkDoneCircle
+                className="cursor-pointer"
+                onClick={() => handleDone(todo.id)}
+              />
+            </div>
+          </form>
         </li>
       ))}
     </div>
